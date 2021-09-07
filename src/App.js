d2346ac96394a274648612c597afb096ee3273d3
Ruslan Rozumnyak <rusroz77@gmail.com>
2021-09-07 10:21:03 +0300

    json_to_form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import questions from './questions.json'
+import {Formik, Field, Form} from 'formik';
+import cn from 'classnames';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [answers, saveAnswers] = useState({});
+    const [answer, setAnswer] = useState(" ");
+    console.log(answer)
+    let show = () => {
+        alert(answers);
+    }
+    let submit = (values) => {
+        let answer = JSON.stringify(values, null, 2);
+        saveAnswers(answer);
+        console.log(answers);
+
+    }
+    return (<div className={"outerContainer"}>
+            <div className={"container"}>
+                <div className={"form"}>
+                    <Formik
+                        initialValues={{questions: questions.questions.question}}
+                        onSubmit={submit}>
+                        <Form>
+                            {questions.questions.map((item, i) => (
+                                <div key={i} className={cn("form__item", "item")}>
+                                    <div className={"item__question"}>
+                                        {item.question}
+                                    </div>
+
+                                    <Field className={"item__answer"} onChange={(e)=>
+                                         setAnswer(e.currentTarget.value)
+                                    } value={answer}
+                                           key={i} id={item.question} name={item.question}
+                                           placeholder={" *Введите текст"}/>
+                                </div>
+                            ))}
+
+                            <button className={"form__button"} type="submit">SEND</button>
+                            <div>
+                                <button className={"form__check"} onClick={show}>CHECK</button>
+                            </div>
+                        </Form>
+                    </Formik>
+
+                </div>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;

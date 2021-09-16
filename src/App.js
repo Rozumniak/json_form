d834603fa7ee082cc5d56a2a75677f1dc94263f5
Ruslan Rozumnyak <rusroz77@gmail.com>
2021-09-16 11:21:53 +0300

    use local storage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import './App.css';
-import React, {useState} from 'react';
+import React from 'react';
 import questions from './questions.json'
 import {Formik, Field, Form} from 'formik';
 import cn from 'classnames';
 
 
 function App() {
-    const [answers, saveAnswers] = useState({});
-    const [answer, setAnswer] = useState(" ");
-    console.log(answer)
-    let show = () => {
-        alert(answers);
-    }
+
     let submit = (values) => {
-        let answer = JSON.stringify(values, null, 2);
-        saveAnswers(answer);
-        console.log(answers);
+        localStorage.setItem('values', JSON.stringify(values));
+        console.log(JSON.parse(localStorage.getItem('values')))
 
     }
+
     return (<div className={"outerContainer"}>
             <div className={"container"}>
                 <div className={"form"}>
                     <Formik
                         initialValues={{questions: questions.questions.question}}
-                        onSubmit={submit}>
+                        onSubmit={(values => submit(values))}>
+                        {({
+                              values,handleChange
+                          }) => (
                         <Form>
                             {questions.questions.map((item, i) => (
                                 <div key={i} className={cn("form__item", "item")}>
@@ -31,19 +29,17 @@ function App() {
                                         {item.question}
                                     </div>
 
-                                    <Field className={"item__answer"} onChange={(e)=>
-                                         setAnswer(e.currentTarget.value)
-                                    } value={answer}
+                                    <Field className={"item__answer"}
                                            key={i} id={item.question} name={item.question}
+                                           value={values.questions}
+                                           onChange={handleChange}
                                            placeholder={" *Введите текст"}/>
                                 </div>
                             ))}
 
                             <button className={"form__button"} type="submit">SEND</button>
-                            <div>
-                                <button className={"form__check"} onClick={show}>CHECK</button>
-                            </div>
                         </Form>
+                        )}
                     </Formik>
 
                 </div>
